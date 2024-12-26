8a6ec5e4e6ef402e82772d40c4d598238e215ae6
olmedix <olmedix@gmail.com>
2024-12-26 18:42:51 +0100

    css profile OK

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -78,30 +78,60 @@ if (error) return <p>Error: {error}</p>;
       <>
       <h2 className="my-6 text-3xl font-semibold">Les meves dades</h2>
 
-      <div className="flex justify-between">
+      <div>
+      <div className="flex justify-between border-b-2 border-stone-500 py-3">
 
-        <div className="w-3/5 bg-slate-500 text-left">
+        <div className="w-3/5 text-left">
+          <h3 className="ml-5 font-semibold text-2xl mb-5">Dades personals</h3>
 
           <h4 className="font-semibold ml-5">Nom</h4>
-          <p className="ml-5">user.data.nombre</p>
+          <p className="ml-5 mb-5">user.data.nombre</p>
 
           <h4 className="font-semibold ml-5">Cognoms</h4>
-          <p className="ml-5">user.data.apellido</p>
+          <p className="ml-5 mb-5">user.data.apellido</p>
 
           <h4 className="font-semibold ml-5">Email</h4>
-          <p className="ml-5">user.data.email</p>
+          <p className="ml-5 mb-5">user.data.email</p>
 
           <h4 className="font-semibold ml-5">Telèfon</h4>
-          <p className="ml-5">user.data.telefono</p>
+          <p className="ml-5 mb-5">user.data.telefono</p>
 
         </div>
         
         <div className="flex items-center">
-          <button className="content-end border-2 font-semibold border-stone-500 py-3 px-14" type="button">
+          <button className="content-end bg-amber-300  transition duration-500 hover:bg-black hover:text-amber-300 border-2 font-semibold border-stone-500 py-3 px-14" type="button">
           <i className="fa-solid fa-pencil mr-1"></i>
             Editar
           </button>
         </div>
+      </div>
+
+      <div className="flex justify-between border-b-2 border-stone-500 py-3">
+
+        <div className="w-3/5 text-left">
+
+        <h3 className="ml-5 font-semibold text-2xl mb-5">Restablir Contrasenya</h3>
+
+          <h4 className="font-semibold ml-5">Contrasenya</h4>
+          <p className="ml-5">**********</p>
+
+        </div>
+        
+        <div className="flex items-center">
+          <button className="content-end bg-amber-300 transition duration-500 hover:bg-black hover:text-amber-300  border-2 font-semibold border-stone-500 py-3 px-14" type="button">
+          <i className="fa-solid fa-pencil mr-1"></i>
+            Editar
+          </button>
+        </div>
+      </div>
+
+        <div className="flex items-center h-20 justify-center gap-16">  
+          <button className="content-end transition duration-500 hover:bg-black hover:text-red-500 bg-red-500 border-2 font-semibold border-stone-500 py-3 px-14" type="button">
+          <i className="fa-solid fa-trash mr-2"></i>
+            Eliminar compte
+          </button>
+        </div>
+
       </div>
       </>
     )

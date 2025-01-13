04aee82ea18966921152ab8346c2b3b5604d68a4
olmedix <olmedix@gmail.com>
2025-01-13 17:51:21 +0100

    sliders agregados

--- src/pages/SpaceDetails.jsx
+++ src/pages/SpaceDetails.jsx
@@ -1,5 +1,17 @@
 import { useLocation} from "react-router-dom";
 
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+
+// import required modules
+import { EffectCoverflow, Pagination } from 'swiper/modules';
+
 //web para buscar los slider para las imagenes
 //https://swiperjs.com/demos?form=MG0AV3#pagination-progress
 
@@ -59,7 +71,7 @@ export default function SpaceDetails(){
                 {space.comments.map((comentario, index) => (
 
                     <div key={index}
-                         className="flex p-5 my-5 text-left rounded-xl"
+                         className="flex p-5 my-5 text-left border-b border-gray-300"
                     >
                         <div className="px-7 font-semibold flex flex-col items-center justify-center">
                             <p className="rounded-full p-2 text-white bg-green-600 border border-green-700 w-12 h-12 flex items-center justify-center">
@@ -80,20 +92,53 @@ export default function SpaceDetails(){
 
                         <p className="font-bold text-xl mb-4">{comentario.comentario}</p>
 
-                        <div className="flex box-border gap-2">
-                            { comentario.imagenes.length > 0 &&
-                                comentario.imagenes.map((imagen, index) => (
-                                    <img 
-                                        key={index} 
-                                        src={imagen.url_imagen} 
-                                        alt={(comentario.usuario)}
-                                        onError={(e) => { 
-                                            e.target.onerror = null;
-                                            e.target.src = "/imgs/modalities/escultura.jpg";
-                                        }}
-                                    />
-                                ))
-                            }
+                        <div className="overflow-hidden w-full box-border">
+                            <Swiper
+                            effect={'coverflow'}
+                            grabCursor={true}
+                            centeredSlides={true}
+                            slidesPerView={'auto'}
+                            coverflowEffect={{
+                              rotate: 50,
+                              stretch: 0,
+                              depth: 100,
+                              modifier: 1,
+                              slideShadows: true,
+                            }}
+                             pagination={true}
+                             modules={[EffectCoverflow, Pagination]}
+                             style={{
+                                overflow: 'hidden',
+                                width: '100%',
+                                maxWidth: '600px',
+                                paddingTop: '50px',
+                                paddingBottom: '50px',
+                              }}
+                            >
+                             {comentario.imagenes.length > 0 &&
+                              comentario.imagenes.map((imagen, index) => (
+                                 <SwiperSlide
+                                  key={index}
+                                   style={{
+                                     backgroundPosition: 'center',
+                                    backgroundSize: 'cover',
+                                     width: '300px',
+                                    height: '300px',
+                                  }}
+                                >
+                                  <img
+                                   src={imagen.url_imagen}
+                                   alt={comentario.usuario}
+                                    style={{ display: 'block', width: '100%' }}
+                                    onError={(e) => {
+                                      e.target.onerror = null;
+                                     e.target.src = '/imgs/modalities/escultura.jpg';
+                                    }}
+                                  />
+                                 </SwiperSlide>
+                               ))}
+                            </Swiper>
+                            
                         </div>
 
                         </div>
@@ -102,10 +147,6 @@ export default function SpaceDetails(){
                     </div>
                 ))}
             </section>
-            
-
-            
-            
         </div>
     );
 }

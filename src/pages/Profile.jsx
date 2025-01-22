0c5039cf6988ebaad801210c31e71328faaeff92
olmedix <olmedix@gmail.com>
2025-01-22 11:02:09 +0100

    control de error al obtener los comentarios del usuario en caso de no haber hecho ninguno

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -12,7 +12,6 @@ export default function Profile(){
     const [comments,setComments]= useState([]);
     const {user,setUser} = useAuth();
     const [loading,setLoading] = useState(false);
-    const [error,setError] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
     const [errorProfile, setErrorProfile] = useState(null);
 
@@ -38,9 +37,9 @@ export default function Profile(){
       try {
         setLoading(true);
         const comments = await fetchGetComments(user.data.id);
-        setComments(comments);
+        if (comments) setComments(comments);
       } catch (err) {
-        setError(err.message);
+        console.error(err.message);
       } finally {
         setLoading(false);
       }
@@ -65,7 +64,6 @@ export default function Profile(){
   };
 
     if (loading || isLoading) return <p>Cargando...</p>;
-    if(error) return <p>{error}</p>;
     if (errorProfile) return <p>{errorProfile}</p>;
 
     return (
@@ -111,41 +109,43 @@ export default function Profile(){
               <ModalForm setUser={setUser} userEmail={userEmail} />
             </div>
           </section>
+
+          {comments.length > 0 && (
+            <section className="border-b-2 border-stone-500 mx-auto py-3">
+              <h3 className="font-semibold text-2xl mb-5">
+                {language === "ca" ? "Els meus comentaris i valoracions" : language === "es" ? "Mis comentarios y valoraciones" : "My comments and ratings"}
+              </h3>
     
-          <section className="border-b-2 border-stone-500 mx-auto py-3">
-            <h3 className="font-semibold text-2xl mb-5">
-              {language === "ca" ? "Els meus comentaris i valoracions" : language === "es" ? "Mis comentarios y valoraciones" : "My comments and ratings"}
-            </h3>
-    
-            <div className="flex items-center justify-center mb-7 font-semibold">
-              <div className="w-1/3 p-4 mr-14 border border-green-800 rounded-lg bg-green-500">
-                {language === "ca" ? "Comentaris confirmats" : language === "es" ? "Comentarios confirmados" : "Confirmed comments"}
+              <div className="flex items-center justify-center mb-7 font-semibold">
+                <div className="w-1/3 p-4 mr-14 border border-green-800 rounded-lg bg-green-500">
+                  {language === "ca" ? "Comentaris confirmats" : language === "es" ? "Comentarios confirmados" : "Confirmed comments"}
+                </div>
+            
+                <div className="w-1/3 p-4 border border-red-800 rounded-lg bg-red-500">
+                  {language === "ca" ? "Comentaris no confirmats" : language === "es" ? "Comentarios no confirmados" : "Unconfirmed comments"}
+                </div>
               </div>
     
-              <div className="w-1/3 p-4 border border-red-800 rounded-lg bg-red-500">
-                {language === "ca" ? "Comentaris no confirmats" : language === "es" ? "Comentarios no confirmados" : "Unconfirmed comments"}
+              <div className="block text-left">
+                  <ul className="rounded-lg">
+                    {comments.map((comment, index) => (
+                      <li
+                        key={index}
+                        className={`mb-3 ${comment.status === 'y' ? 'text-green-600' : 'text-red-500'}`}
+                      >
+                        <span className="text-yellow-500 font-semibold pr-5">
+                          {comment.score}
+                          <i className="fa-solid fa-star text-xl text-yellow-500"></i>
+                        </span>
+                        <span className="font-bold pr-2">{comment.space}:</span>
+                        {comment.comment}
+                      </li>
+                    ))}
+                  </ul>   
               </div>
-            </div>
-    
-            <div className="block text-left">
-              <ul className="rounded-lg">
-                {comments.map((comment, index) => (
-                  <li
-                    key={index}
-                    className={`mb-3 ${comment.status === 'y' ? 'text-green-600' : 'text-red-500'}`}
-                  >
-                    <span className="text-yellow-500 font-semibold pr-5">
-                      {comment.score}
-                      <i className="fa-solid fa-star text-xl text-yellow-500"></i>
-                    </span>
-                    <span className="font-bold pr-2">{comment.space}:</span>
-                    {comment.comment}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </section>
-    
+            </section>
+          )}
+          
           <section className="flex items-center h-20 justify-center gap-16">
             <button
               className="content-end transition duration-500 hover:bg-black hover:text-red-500 bg-red-500 border-2 font-semibold border-stone-500 py-3 px-14"
